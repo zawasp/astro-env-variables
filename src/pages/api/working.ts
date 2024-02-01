8c40d3c73b957a207178e49b641bb66f25f5bb69
zawasp <mircea.rila@gmail.com>
2024-02-01 14:10:47 +0200

    added another route

---
+++ src/pages/api/working.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+import { type FlarebaseConfig, FlarebaseAuth } from "@marplex/flarebase-auth";
+
+
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+    const authConfig: FlarebaseConfig = {
+        apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+        projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+        privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        serviceAccountEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
+      };
+    const serverAuth = new FlarebaseAuth(authConfig);
+    return new Response("ok", { status: 200 });
+};
