d3a4b2f0b1ae1ecbc959f77f3201c9c46c7582fa
zawasp <mircea.rila@gmail.com>
2024-02-01 13:22:16 +0200

    ssr route

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro';
 
+export const prerender = false;
+
 const apiKey = import.meta.env.API_KEY;
 console.log(apiKey);
 export const GET: APIRoute = async () => {
